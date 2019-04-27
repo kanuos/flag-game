8bbd0a52b0e3fc05bd9c71d3292c9a80cf873257
sounak <sounakmukherjee@ymail.com>
2019-04-27 11:29:32 -0400

    Flag game uploaded to GH.. api to be incorporated

---
+++ app.js
@@ -0,0 +1,30 @@
+
+document.getElementById('text-box').innerText='';
+// Iran
+var box1=document.getElementById('box-1');
+box1.addEventListener('mouseenter',()=>{
+    document.getElementById('text-box').innerText='IRAN';
+});
+box1.addEventListener('mouseleave',()=>{
+    document.getElementById('text-box').innerText='';
+});
+
+//iraq
+var box2=document.getElementById('box-2');
+box2.addEventListener('mouseenter',()=>{
+    document.getElementById('text-box').innerText='IRAQ';
+});
+box2.addEventListener('mouseleave',()=>{
+    document.getElementById('text-box').innerText='';
+});
+
+//israel
+
+var box3=document.getElementById('box-3');
+box3.addEventListener('mouseenter',()=>{
+    document.getElementById('text-box').innerText='ISRAEL';
+});
+box3.addEventListener('mouseleave',()=>{
+    document.getElementById('text-box').innerText='';
+});
+
